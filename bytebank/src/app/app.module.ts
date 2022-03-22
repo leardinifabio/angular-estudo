e904582d00a872645b07fbc0ff9b8c46d3e71147
Fabio Leardini <leardinifabio90@gmail.com>
2022-03-22 00:41:38 -0300

    04.Transferências

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
-  declarations: [AppComponent, NovaTranferenciaComponent],
+  declarations: [AppComponent, NovaTranferenciaComponent, ExtratoComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
